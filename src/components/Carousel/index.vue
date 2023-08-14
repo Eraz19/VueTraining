0a4ffbc8cfa3c758128f98de99f89ffb8b5b7da4
alexander douieb <alexander.douieb@ledr.io>
2023-08-14 12:31:51 +0200

    implemented scrolling movement without animation transition

--- src/components/Carousel/index.vue
+++ src/components/Carousel/index.vue
@@ -8,22 +8,25 @@
 	const props = defineProps<T_CarouselProps>();
 
 	const carouselNames : Ref<string[]> = ref(props.elementsNames);
+	const scrollingSide : Ref<"left"|"right"|undefined> = ref();
 
 	watchEffect(() => { carouselNames.value = props.elementsNames; });
 
 	function ScrollLeft() : void
 	{
-		const firstElement : string|undefined = carouselNames.value.shift();
+		scrollingSide.value = "left";
+		/*const firstElement : string|undefined = carouselNames.value.shift();
 
 		if (firstElement)
-			carouselNames.value.push(firstElement);
+			carouselNames.value.push(firstElement);*/
 	};
 	function ScrollRight() : void
 	{
-		const lastElement : string|undefined = carouselNames.value.pop();
+		scrollingSide.value = "right";
+		/*const lastElement : string|undefined = carouselNames.value.pop();
 
 		if (lastElement)
-			carouselNames.value.unshift(lastElement);
+			carouselNames.value.unshift(lastElement);*/
 	};
 </script>
 
@@ -37,8 +40,10 @@
 			:class="[$style.arrow, $style.right]"
 			@click="ScrollRight"
 		/>
-		<div :class="$style.wrapper">
-			<slot v-for="name in carouselNames" :name=name></slot>
+		<div :class="$style.wrapper" ref="element">
+			<div :class="[$style.inner, (scrollingSide != undefined) ? $style[scrollingSide] : undefined]">
+				<slot v-for="name in carouselNames" :name=name></slot>
+			</div>
 		</div>
 	</div>
 </template>
@@ -46,8 +51,7 @@
 <style module lang="scss">
 	.container
 	{
-		--gap       : v-bind('props.gap + "px"');
-		--numElement: v-bind(props.numberElement);
+		--gap: v-bind('props.gap + "px"');
 
 		position   : relative;
 		height     : 100%;
@@ -71,26 +75,31 @@
 	};
 	.arrow.left  { left: 10px; };
 	.arrow.right { right: 10px; };
-	/*.inner
-	{
-		box-sizing: border-box;
-		border    : rgb(217, 255, 0) solid 2px;
-		//width     : 100%;
-		height    : 80%;
-		white-space: "nowrap";
-	};*/
 	.wrapper
 	{
-		transition: all 2s;
-		box-sizing       : border-box;
-		border           : blue solid 2px;
-		width            : 100%;
-		height           : 80%;
-		margin           : 0 var(--gap);
-		display          : grid;
-		grid-auto-flow   : column;
-		grid-auto-columns: calc( (100% / var(--numElement)) - (var(--gap) * ((var(--numElement) - 1) / var(--numElement)) ));
-		column-gap       : var(--gap);
+		position: relative;
+		box-sizing: border-box;
+		border    : blue solid 2px;
+		width     : 100%;
+		height    : 80%;
+		margin    : 0 var(--gap);
 	};
+	.inner
+	{
+		--numVisibleElement: v-bind(props.numberElement);
+		--numVisibleGaps   : calc(var(--numVisibleElement) - 1);
+		--totalNumOfElement: v-bind(carouselNames.length);
+		--totalNumberGaps  : calc(v-bind(carouselNames.length) - 1);
+
+		position: absolute;
+		box-sizing: border-box;
+		border    : rgb(217, 255, 0) solid 2px;
+		width     : calc((((100% - (var(--gap) * var(--numVisibleGaps))) / var(--numVisibleElement)) * var(--totalNumOfElement)) + (var(--totalNumberGaps) * var(--gap)));
+		height    : 100%;
+		display   : flex;
+		gap       : var(--gap);
+	};
+	.inner.left  { transform: translateX( calc( (-100% + (var(--totalNumberGaps) * var(--gap)) ) / var(--totalNumOfElement) - var(--gap)) ) };
+	.inner.right { transform: translateX( calc( ( 100% - (var(--totalNumberGaps) * var(--gap)) ) / var(--totalNumOfElement) + var(--gap)) ) };
 </style>
 
